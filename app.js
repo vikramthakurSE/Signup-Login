8322a3cd02cc6cf3e60d146c7ff2485e549ad5fe
Vikram <vikramthakur381@gmail.com>
2021-07-27 03:17:26 +0530

    hashing and salting password

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 require("./db/conn");
@@ -31,32 +32,41 @@ app.get("/login", (req,res) =>{
 
 
 app.post("/register", (req, res) => {
-    const newUser = new User ({
-        email: req.body.email,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User ({
+            email: req.body.email,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else {
+                res.render("secrets");
+            }
+        });
     });
+    
 });
 
 app.post("/login", (req, res) => {
     const email = req.body.email;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: email}, function(err, foundUser){
         if(err) {
             console.log(err);
         } else{
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets");
-                }
+                // Load hash from your password DB.
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });         
             }
         }       
     });
